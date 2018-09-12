d185f1b94fb426754e959c571d81d61b3eddde31
felipepestana18 <felipe-pe27@hotmail.com>
2018-09-12 11:07:07 -0300

    first comit - send project

---
+++ BuscaCepAspNet/BuscaCepAspNet/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BuscaCepAspNet.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuscaCepAspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string CepBuscado = ""; //Cep a Ser Buscado.
+            string url = "https://viacep.com.br/ws/" + CepBuscado + "/json/"; //Montagem da url
+
+            WebRequest Requisicao = WebRequest.Create(url);
+            WebResponse Resposta = Requisicao.GetResponse();
+
+            StreamReader Leitor = new StreamReader(Resposta.GetResponseStream());
+            string RespostaString = Leitor.ReadToEnd();
+            Leitor.Close();
+
+            ClasseCep ObjetoCep = JsonConvert.DeserializeObject<ClasseCep>(RespostaString);
+
+            return View(ObjetoCep);
+        }
+    }
+}
---
+++ BuscaCepAspNet/BuscaCepAspNet/Models/ClasseCep.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuscaCepAspNet.Models
+{
+    public class ClasseCep
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string unidade { get; set; }
+        public int ibge { get; set; }
+        public int gia { get; set; }
+    }
+}
